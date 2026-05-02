a21aa7d57f4e4fceb96bd0f298aa853b235859e4
kaycodes13 <egad.coding@gmail.com>
2026-05-02 17:43:09 -0400

    break layout into templates, update fonts

--- public/main.js
+++ public/main.js
@@ -11,20 +11,4 @@ export default {
       title: "Discord",
     },
   ],
-  start: () => {
-    /* The logo's alt text is redundant because of the site title directly beside it, and
-    the image is purely decorative. So we hide it from screenreaders. */
-    let logo = document.body.querySelector("#logo");
-    logo.ariaHidden = true;
-    logo.alt = "";
-
-    // docfx doesn't provide a skip-to-content link. :/
-
-    let skipLink = document.createElement("a");
-    skipLink.id = "skip-to-content";
-    skipLink.href = "#" + document.body.querySelector("main h1").id;
-    skipLink.innerText = "skip to main content";
-
-    document.body.querySelector("header").prepend(skipLink);
-  },
 };
