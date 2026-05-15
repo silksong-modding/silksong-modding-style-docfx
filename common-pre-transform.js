aaaa35b701c0b566d4e2950278b8795df555c87e
Kaykao <32107308+kaycodes13@users.noreply.github.com>
2026-05-15 15:34:23 -0400

    HTML Templating and Accessibility Improvements (#2)
    * break layout into templates, update fonts
    
    * fix and style search results
    
    * optimize resource loading a bit
    
    preloading docfx's big dom-manipulating script and some of the theme's images should help reduce layout shifting as the page loads and cause the dynamically-added navigational links to appear slightly sooner.
    
    * reduce CLS, repair theme menu accessibility
    
    I really cannot stress enough how lacking the keyboard/screen reader accessibility of the theme picker was, or how annoying it is that it's dynamically generated and thus can't be templated without replacing the entire docfx-modern dynamic content system.
    
    * improve navbar dropdown semantics
    
    know that if i had it in me i would be replacing the entire dynamic content generator for the nav bar right now
    
    * further improve navbar semantics
    
    * overhaul dynamic navigation generation
    
    * add ToC filtering back in
    
    * add pdf support back in
    
    * fix active link highlighting, improve scroll behaviour
    
    * split main.scss into several partials
    
    * split main landmark styles into several partials
    
    * overhaul search function, misc improvements
    
    * minify javascript, update README
    
    * fix minor visual glitch with top fleur

--- common-pre-transform.js
+++ common-pre-transform.js
@@ -2,6 +2,6 @@ exports.addGlobalMetadata = function (model) {
   model._appLogoPath = "public/spagnet.svg";
   model._appFaviconPath = "public/spagnet.ico";
   model._appLogoUrl = "https://docs.silksong-modding.org";
-  model._lang = "en"
+  model._lang = "en";
   return model;
 };
